432f94f981b42c5fb3a32e0c05eb3ec2a0421972
Alex Yang <aleozlx@gmail.com>
2017-04-28 14:15:32 -0500

    init

---
+++ build/CMakeFiles/opengl_0.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opengl_0.dir/main.cpp.o"
+  "opengl_0.pdb"
+  "opengl_0"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/opengl_0.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
